046b58ac188b15468fc629212b2534d3e6d3d893
HxHexa <tquang1708@gmail.com>
2019-08-29 09:00:36 -0400

    a

---
+++ commands/globalvar.py
@@ -0,0 +1,10 @@
+#global variables for interacting with all commands 
+#to store data accross sessions
+
+#Used to keep track of all sources and categories through sessions
+#To be pickled/depickled at start of program
+masterSource = {}
+masterCate = {}
+#Used for commands that require listing of all the sources/cate
+listStrSource = ''
+listStrCate = ''
